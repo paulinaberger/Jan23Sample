80831fc2d9f80081f494c44ebf891403479ba099
Paulina Berger <paulinaberger@a502.aules.local>
2017-01-23 11:57:03 +0100

    Commiting changes that we're doing - adding first activity from scratch

--- app/src/main/java/org/mdsd2016/android/newproject/MainActivity.java
+++ app/src/main/java/org/mdsd2016/android/newproject/MainActivity.java
@@ -1,13 +1,19 @@
 package org.mdsd2016.android.newproject;
 
+import android.app.Activity;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+    /**
+     * Created by paulinaberger on 2017-01-23.
+     */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends Activity {
+        @Override
+        public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+            super.onCreate(savedInstanceState, persistentState);
+
+            setContentView(R.layout.activity_main2);
+        }
     }
-}
